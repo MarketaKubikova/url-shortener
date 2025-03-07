32adf4b654d28b875c944fe8324db55af90aa50e
Markéta Kubíková <73794455+MarketaKubikova@users.noreply.github.com>
2025-03-07 22:43:23 +0100

    created custom url validation (#3)

--- src/main/java/com/urlshortener/dto/UrlShortenedRequestDto.java
+++ src/main/java/com/urlshortener/dto/UrlShortenedRequestDto.java
@@ -1,5 +1,7 @@
 package com.urlshortener.dto;
 
+import com.urlshortener.validation.ValidUrl;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,5 +14,7 @@ public class UrlShortenedRequestDto {
     /**
      * The original URL to be shortened.
      */
+    @NotEmpty(message = "Original URL cannot be empty")
+    @ValidUrl
     private String originalUrl;
 }
